887ccb844a1e4e627cacdbcb22395ab0f55f4b99
gpamariner <gpamariner@gmail.com>
2025-07-03 12:50:46 +0000

    i18n update 5

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "../globals.css";
+import "@/app/globals.css";
 import { AppHeader } from "@/components/app-header";
 import { Toaster } from "@/components/ui/toaster";
 
